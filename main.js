5983ff7f5683ea4042917c6da7eff3c93b8bb87e
prashanth <reddy8129@gmail.com>
2022-05-24 18:42:07 +0530

    added console.log

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 console.log("Hello this is for git learning");
 console.log("this line added in branch feature-a");
-console.log("added in branch new-feature"); 
+console.log("added in branch main"); 
